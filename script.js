0e2c68849662d9f45d2797be400225aa5bdeb802
Max Nikolenko <maxim.nikolenko@gmail.com>
2017-10-11 00:00:27 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var voice = undefined;
+
+function loadTheVoice() {
+  var voices = window.speechSynthesis.getVoices();
+
+  voice = voices.filter(function(voice) { return voice.name === 'Google US English'; })[0];
+}
+
+loadTheVoice();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = loadTheVoice;
+}
+
+var exercise = document.querySelector('#exercise');
+
+exercise.onclick = function(event) {
+	if (event.target.tagName === 'IMG') {
+		var speech = new SpeechSynthesisUtterance(event.target.nextSibling.innerText);
+
+		speech.voice = voice;
+		speech.lang = 'en-US';
+		window.speechSynthesis.speak(speech);
+	}
+}
